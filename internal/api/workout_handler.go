3403aea7bc15822d41648c64a3fd601ae6e694fa
Joshua D. Mukiibi <joshuamukiibi10@gmail.com>
2025-05-11 16:32:36 +0300

    Add workout creation and storage with Postgres integration
    Introduce WorkoutStore interface and Postgres implementation to handle workout persistence. Updated WorkoutHandler to use the store, enabling the creation of workouts via HTTP requests. Integrated the handler and store into the application setup.

--- internal/api/workout_handler.go
+++ internal/api/workout_handler.go
@@ -1,16 +1,22 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"github.com/jdmukiibs/femProject/internal/store"
 	"net/http"
 	"strconv"
 )
 
-type WorkoutHandler struct{}
+type WorkoutHandler struct {
+	workoutStore store.WorkoutStore
+}
 
-func NewWorkoutHandler() *WorkoutHandler {
-	return &WorkoutHandler{}
+func NewWorkoutHandler(workoutStore store.WorkoutStore) *WorkoutHandler {
+	return &WorkoutHandler{
+		workoutStore: workoutStore,
+	}
 }
 
 func (wh *WorkoutHandler) HandleGetWorkoutById(w http.ResponseWriter, r *http.Request) {
@@ -30,5 +36,20 @@ func (wh *WorkoutHandler) HandleGetWorkoutById(w http.ResponseWriter, r *http.Re
 }
 
 func (wh *WorkoutHandler) HandleCreateWorkout(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "created a workout\n")
+	var workout store.Workout
+	err := json.NewDecoder(r.Body).Decode(&workout)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "failed to create workout", http.StatusInternalServerError)
+		return
+	}
+
+	createdWorkout, err := wh.workoutStore.CreateWorkout(&workout)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "failed to create workout", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(createdWorkout)
 }
